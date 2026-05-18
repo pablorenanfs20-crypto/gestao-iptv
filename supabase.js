c8bf602920b487034f41554c362034a6cdb6829d
pablorenanfs20-crypto <pablorenanfs20@gmail.com>
2026-05-18 10:51:18 -0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = "https://tfhwwasxecqkyccprupc.supabase.co";
+
+const SUPABASE_KEY = "SUA_ANON_KEY_AQUI";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
