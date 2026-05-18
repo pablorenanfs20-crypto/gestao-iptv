9f35796c3459564b8043fb5966e3e31a3d01458a
pablorenanfs20-crypto <pablorenanfs20@gmail.com>
2026-05-18 10:52:08 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://tfhwwasxecqkyccprupc.supabase.co";
 
-const SUPABASE_KEY = "SUA_ANON_KEY_AQUI";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmaHd3YXN4ZWNxa3ljY3BydXBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwNDY0MTQsImV4cCI6MjA5NDYyMjQxNH0.5K-tzFe7DuUT7nQqJOd183nHyCYD3g979xoLH-VA0qw";
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
